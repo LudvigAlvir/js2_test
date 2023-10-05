948aee5ff285517100d7ad1f22316535c6f1654d
ErikHHJ <erikhhju@gmail.com>
2023-10-05 15:18:23 +0200

    Feed update

--- feed/feed.js
+++ feed/feed.js
@@ -1,14 +1,8 @@
 const url = "https://api.noroff.dev/api/v1/social/posts";
 const row = document.querySelector(".row");
 var likeBtn = document.querySelectorAll('.like')
-likeBtn.forEach(function(btn) {
+const API_BASE_URL = 'https://api.noroff.dev';
 
-  btn.addEventListener('click', function() {
-    btn.style.backgroundColor = "red";
-    console.log("hello");
-    
-  })
-})
 //example get request for ca
 async function getPosts() {
 	const accessToken = localStorage.getItem("accessToken");
@@ -23,12 +17,20 @@ async function getPosts() {
 	console.log(data);
   
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 100; i++) {
     const oldTime = data[i].created;
-    const newTime = calculateMinutesAgo(oldTime);
-    if(data[i].media === "null"){
+    let newTime = Number(calculateMinutesAgo(oldTime));
+    
+    if(newTime > 60){
+     newTime = Math.round(newTime / 60) + " hours ago"
+    }else if(Math.round(newTime / 60) > 24) {
+      newTime = Math.round((newTime / 60) / 24) + " days ago";
+    } else if (newTime < 60){
+      newTime = newTime + " minutes ago"
+    }
+    if(!data[i].media){
       continue
-    } else {
+    } 
       const postDiv = document.createElement("div");
       postDiv.classList.add(
       "col-lg-5",
@@ -41,7 +43,7 @@ async function getPosts() {
       "align-items-center"
     );
     postDiv.innerHTML += `
-      <p class="fw-lighter">${newTime} minutes ago</p>
+      <p class="fw-lighter">${newTime}</p>
       <img class="card-img-top object-fit-fill" src="${data[i].media}">
       <div class="card-body">
         <h5 class="card-title">${data[i].title}</h5>
@@ -53,10 +55,17 @@ async function getPosts() {
         </div>`;
   
     row.appendChild(postDiv);
-    }
+    
+    var likeBtn = document.querySelectorAll('.like')
+    likeBtn.forEach(function(btn) {
   
+      btn.addEventListener('click', function() {
+        btn.style.backgroundColor = "red";
+      })
+    })
     
   }
+  
 }
 getPosts()
 
@@ -68,6 +77,50 @@ function calculateMinutesAgo(timestampString) {
   const minutesDifference = Math.floor(timeDifferenceMillis / (1000 * 60));
   return minutesDifference;
 }
+
+const postBtn = document.querySelector("#postbtn");
+const titleInput = document.querySelector("#titleinput");
+const bodyInput = document.querySelector("#bodyinput");
+const tagsInput = document.querySelector("#tagsinput")
+const mediaInput = document.querySelector("#mediainput");
+const modal = document.querySelector("#static-backdrop");
+
+
+
+async function newPost(url,info) {
+	const accessToken = localStorage.getItem("accessToken");
+	const res = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${accessToken}`, // what we use for authentication
+		}, body: JSON.stringify(info),
+	});
+	const data = await res.json();
+	console.log(data);
+  location.reload()
+}
+postBtn.addEventListener("click", ()=>{ 
+  
+ const arr = tagsInput.value.split(" ")
+ console.log(arr);
+  const postInfo = {
+      title: `${titleInput.value}`,
+      body: `${bodyInput.value}`,
+      tags: arr,
+      media: `${mediaInput.value}`,
+  }
+  console.log(postInfo);
+  newPost(`${API_BASE_URL}/api/v1/social/posts`, postInfo);
+  
+
+  
+  
+});
+
+
+
+
 
 
 
