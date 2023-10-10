de0b86f212c0f84c1e4f82e92503357b20812d4d
ErikHHJ <erikhhju@gmail.com>
2023-10-10 13:27:24 +0200

    Merged via copy paste

--- profile/profile.js
+++ profile/profile.js
@@ -1,140 +1,171 @@
 const API_BASE_URL = "https://api.noroff.dev";
 const url = "https://api.noroff.dev/api/v1/social/profiles/";
+let accessToken;
+const displayedName = localStorage.getItem("username");
+accessToken = localStorage.getItem("accessToken");
+let newBannerImageUrl = "";
 
 async function fetchUserProfile() {
-    const displayedName = localStorage.getItem("username")
-    const accessToken = localStorage.getItem("accessToken");
 
-    console.log(`${API_BASE_URL}/social/profiles/posts`);
-    console.log(displayedName)
-    console.log(accessToken);
-    console.log(url+displayedName)
+  try {
+    const response = await fetch(url + displayedName, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-    try {
-      const response = await fetch(url + displayedName, {
-        method: "get",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error("Failed to fetch user profile");
-      }
-    
-      const userData = await response.json();
-
-      console.log(userData);
-  
-      updateProfileSection(userData);
-    } catch (error) {
-      console.error("Error fetching profile:", error);
+    if (!response.ok) {
+      throw new Error("Failed to fetch user profile");
     }
-  };
 
+    const userData = await response.json();
 
-
-
-  function updateProfileSection(userData) {
-    document.querySelector(".ms-3 h5").textContent = userData.name;
-  
-    const avatarImg = document.querySelector(".img-fluid.img-thumbnail");
-    avatarImg.src = userData.avatar;
-
-    document.querySelector(".mb-1.h5").textContent = userData.posts.length;
-    document.querySelectorAll(".small.mb-0")[0].textContent = userData._count.followers;
-    document.querySelectorAll(".small.mb-0")[1].textContent = userData._count.following;
+    updateProfile(userData);
+  } catch (error) {
+    console.error("Error fetching profile:", error);
   }
+}
 
-const profileDiv = document.createElement("div");
-profileDiv.innerHTML += `
-
-<section class="h-100 gradient-custom-2">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col col-lg-9 col-xl-7">
-            <div class="card">
-              <div
-                class="rounded-top text-white d-flex flex-row"
-                style="background-color: #000; height: 200px"
-              >
-                <div class="ms-4 mt-5 d-flex flex-column" style="width: 150px">
-                  <img
-                    src="${this.avatarImg}"
-                    alt=""
-                    class="img-fluid img-thumbnail mt-4 mb-2"
-                    style="width: 150px; z-index: 1"
-                  />
-                  <button
-                    type="button"
-                    class="btn btn-outline-dark"
-                    data-mdb-ripple-color="dark"
-                    style="z-index: 1"
-                  >
-                    Edit profile
-                  </button>
+function updateProfile(userData) {
+  const profileDiv = document.createElement("div");
+  profileDiv.innerHTML = `
+  <section class="h-100 gradient-custom-2">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-lg-9 col-xl-7">
+          <div class="card">
+            <div class="rounded-top text-white d-flex flex-row" style="background-color: #000; height: 220px">
+              <div class="ms-4 mt-5 d-flex flex-column" style="width: 150px">
+                <img src="../media/unknown_picture.jpg" alt="unknown profile" class="img-fluid img-thumbnail mt-4 mb-2" style="width: 150px; z-index: 1" />
+                <button type="button" id="editProfileButton" class="btn btn-outline-dark" data-mdb-ripple-color="dark" style="z-index: 1">
+                  Edit profile
+                </button>
+              </div>
+              <div class="ms-3" style="margin-top: 130px">
+                <h5>${userData.name}</h5>
+                <p>${userData.location}</p>
+              </div>
+            </div>
+            <div class="p-4 text-black" style="background-color: #f8f9fa">
+              <div class="follow-container">
+                <button id="followButton" class="btn btn-primary">
+                  Follow
+                </button>
+              </div>
+              <div class="d-flex justify-content-end text-center py-1">
+                <div>
+                  <p class="mb-1 h5">${userData._count.posts}</p>
+                  <p class="small mb-0">Posts</p>
                 </div>
-                <div class="ms-3" style="margin-top: 130px">
-                  <h5>${this.name}</h5>
-                  <p>${this.location}</p>
+                <div class="px-3">
+                  <p class="mb-1 h5">${userData._count.followers}</p>
+                  <p class="small mb-0">Followers</p>
+                </div>
+                <div>
+                  <p class="mb-1 h5">${userData._count.following}</p>
+                  <p class="small mb-0">Following</p>
                 </div>
               </div>
-              <div class="p-4 text-black" style="background-color: #f8f9fa">
-                <div class="d-flex justify-content-end text-center py-1">
-                  <div>
-                    <p class="mb-1 h5">253</p>
-                    <p class="small mb-0">${this.posts}</p>
-                  </div>
-                  <div class="px-3">
-                    <p class="mb-1 h5">1026</p>
-                    <p class="small mb-0">${this._followers}</p>
-                  </div>
-                  <div>
-                    <p class="mb-1 h5">478</p>
-                    <p class="small mb-0">${this._following}</p>
-                  </div>
-                </div>
-              </div>
-              <div class="card-body p-4 text-black">
-                <!--  <div class="mb-5">
-                  <p class="lead text-white fw-normal mb-1">About</p>
-                  <div class="p-4" style="background-color: #f8f9fa">
-                    <p class="font-italic mb-1">Web Developer</p>
-                    <p class="font-italic mb-1">Fellow Pokemon Enjoyer</p>
-                    <p class="font-italic mb-0">Lives in Bergen, Norway</p>
-                  </div>
-                </div> -->
-                <div
-                  class="d-flex justify-content-between align-items-center mb-4"
-                >
-                  <p class="text-white lead fw-normal mb-0">Recent posts</p>
-                  <p class="mb-0">
-                    <a href="#!" class="text-muted">Show all</a>
-                  </p>
-                </div>
-                <div class="row g-2">
-                  <div class="col mb-2">
-                    <img
-                      src=""
-                      alt=""
-                      class="w-100 rounded-3"
-                    />
-                  </div>
-                  <div class="col mb-2">
-                    <img
-                      src=""
-                      alt=""
-                      class="w-100 rounded-3"
-                    />
-                  </div>
-                </div>
-              </div>
+            </div>
+            <div class="card-body p-4 text-black">
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
+
+  <div id="editProfileModal" class="modal">
+  <div class="modal-content">
+    <span id="closeModal" class="close">&times;</span>
+    <h2>Edit Profile</h2>
+    <label for="bannerImageInput">New Banner Image URL:</label>
+    <input type="text" id="bannerImageInput" name="bannerImageInput" placeholder="Enter new banner image URL">
+    <button id="saveBannerButton">Save Banner Image</button>
+    <img id="bannerImage" src="${newBannerImageUrl}" alt="Banner Image">
+  </div>
+</div>
+
+
 `;
 
+  document.body.appendChild(profileDiv);
+
+
+  editProfileButton.addEventListener("click", () => {
+    const editProfileModal = document.getElementById("editProfileModal");
+    editProfileModal.style.display = "block";
+  
+    const bannerImageInput = document.getElementById("bannerImageInput");
+    const saveBannerButton = document.getElementById("saveBannerButton");
+    const bannerImage = document.getElementById("bannerImage");
+  
+    saveBannerButton.addEventListener("click", () => {
+      const newBannerImageUrl = bannerImageInput.value;
+  
+
+      bannerImage.src = newBannerImageUrl;
+  
+      editProfileModal.style.display = "none";
+    });
+  
+
+    const closeModalButton = document.getElementById("closeModal");
+    closeModalButton.addEventListener("click", () => {
+      editProfileModal.style.display = "none";
+    });
+  });
+  
+  
+  
+  
+
+
+    const followButton = document.getElementById("followButton");
+
+    followButton.addEventListener("click", async () => {
+    try {
+      const isFollowing = Array.isArray(userData.following) && userData.following.some(profile => profile.name === displayedName);
+  
+      const endpoint = isFollowing
+        ? `${API_BASE_URL}/social/profiles/${userData.name}/unfollow`
+        : `${API_BASE_URL}/social/profiles/${userData.name}/follow`;
+  
+      const response = await fetch(endpoint, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to follow/unfollow profile");
+      }
+  
+      if (isFollowing) {
+        followButton.textContent = "Follow";
+      } else {
+        followButton.textContent = "Unfollow";
+      }
+    } catch (error) {
+      console.error("Error following/unfollowing profile:", error);
+    }
+  });
+}
+
 fetchUserProfile();
+
+
+
+/* 
+Vise posts under brukeren 
+
+kunne redigere en post 
+
+kunne slette en post 
+ */
+
+ 
